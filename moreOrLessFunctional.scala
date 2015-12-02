b5da8657b1f6ef3b194820cc735ae8a01c022fcc
Kim Lavoie <kim.lavoie.14@gmail.com>
2015-12-02 16:33:22 -0500

    Added tests

--- moreOrLessFunctional.scala
+++ moreOrLessFunctional.scala
@@ -9,28 +9,45 @@ def getResultMessage(guess:Int, numberToGuess:Int) = guess match {
 }
 
 @tailrec
-def play(numberToGuess:Int, upperBound:Int){
-    print(s"Guess a number between 0 and ${upperBound}: ")
-    val guess = readInt
+def play(numberToGuess:Int, upperBound:Int, input: () => String = readLine, output: String => Unit = print){
+    output(s"Guess a number between 0 and ${upperBound}: ")
+    val guess = input().toInt
     val message = getResultMessage(guess, numberToGuess)
-    println(message)
-    if(guess != numberToGuess) play(numberToGuess, upperBound)
+    output(message + "\n")
+    if(guess != numberToGuess) play(numberToGuess, upperBound, input, output)
 }
 
 @tailrec
-def gameLoop(){
-    println("Welcome to the game of More or Less!")
-    print("Find the random number between 0 and X, where X equals: ")
-    val upperBound = readInt
-    val numberToGuess = new Random().nextInt(upperBound)
+def gameLoop(input: () => String = readLine, output: String => Unit = print, random: Int => Int = new Random().nextInt){
+    output("Welcome to the game of More or Less!\n")
+    output("Find the random number between 0 and X, where X equals: ")
+    val upperBound = input().toInt
+    val numberToGuess = random(upperBound) //new Random().nextInt(upperBound)
 
-    play(numberToGuess, upperBound)
+    play(numberToGuess, upperBound, input, output)
 
-    print("Play again? (y/n): ")
-    val answer = readLine
-    if (answer != "y") println("Thank you for playing!")
-    else gameLoop()
+    output("Play again? (y/n): ")
+    val answer = input()
+    if (answer != "y") output("Thank you for playing!\n")
+    else gameLoop(input, output, random)
 }
-    
+
+def testGameLoop(){
+    def assert(condition:Boolean) = if(condition) println("Test pass") else println("Test fail")
+    val inputs = Array("10", "9", "1", "5", "y", "10", "5", "n")
+    var i = 0
+    def input() = {
+        val return_value = inputs(i)
+        i += 1
+        return_value
+    }
+    def output(str:String): Unit = return
+    def random(i:Int) = 5
+    gameLoop(input, output, random)
+    assert(i == inputs.length)
+}
+
+testGameLoop()
 gameLoop()
+    
 
